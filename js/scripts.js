f7376de9a5651abc6495f14b5a7e01e19aa861c3
Mohd Khizar Rasheed <mkhizarrash7861@gmail.com>
2024-03-02 21:46:27 +0530

    changes

---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
+
+
+const testimonialsContainer = document.querySelector(".testimonials-container");
+const testimonial = document.querySelector(".testimonial");
+const userImage = document.querySelector(".user-image");
+const username = document.querySelector(".username");
+const role = document.querySelector(".role");
+
+const testimonials = [
+  {
+    name: "suresh",
+    position: "",
+    photo:
+      "./assets/img/portfolio/testimonials/2.jpg",
+    text:
+      "Reliable and trustworthy. Muthoot Finance's gold loan service provided the financial assistance I needed promptly. Clear terms and friendly customer service."
+  },
+  {
+    name: "Rajesh",
+    position: "",
+    photo: "./assets/img/portfolio/testimonials/3.jpg",
+    text:
+      "Great experience with Muthoot Finance. Their microfinance initiative is making a positive impact in the community. Transparent policies and excellent customer support."
+  },
+  {
+    name: "Raghav",
+    position: "",
+    photo: "./assets/img/portfolio/testimonials/4.jpg",
+    text:
+      "Efficient money transfer services by Muthoot Finance. Quick and secure transactions. Competitive rates and excellent customer care. A reliable choice for remittances."
+  }
+];
+
+let idx = 1;
+
+function updateTestimonial() {
+  const { name, position, photo, text } = testimonials[idx];
+
+  testimonial.innerHTML = text;
+  userImage.src = photo;
+  username.innerHTML = name;
+  role.innerHTML = position;
+
+  idx++;
+
+  if (idx > testimonials.length - 1) {
+    idx = 0;
+  }
+}
+
+setInterval(updateTestimonial, 10000);
+
